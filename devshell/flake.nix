0afed4c1efd181ed4c022ecc37ba57b07e1de5a2
David Arnold <david.arnold@iohk.io>
2022-08-14 15:09:05 -0500

    chore: update devshell

--- devshell/flake.nix
+++ devshell/flake.nix
@@ -2,9 +2,6 @@
   description = "Yants devshell";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.devshell.url = "github:numtide/devshell";
-  inputs.treefmt.url = "github:numtide/treefmt";
-  inputs.alejandra.url = "github:kamadorueda/alejandra";
-  inputs.alejandra.inputs.treefmt.url = "github:divnix/blank";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs =
     inputs:
@@ -13,15 +10,13 @@
       let
         devshell = inputs.devshell.legacyPackages.${system};
         nixpkgs = inputs.nixpkgs.legacyPackages.${system};
-        alejandra = inputs.alejandra.defaultPackage.${system};
-        treefmt = inputs.treefmt.defaultPackage.${system};
       in
         {
           devShells.__default = devshell.mkShell {
             name = "Yants";
             packages = [
-              alejandra
-              treefmt
+              nixpkgs.alejandra
+              nixpkgs.treefmt
               nixpkgs.shfmt
               nixpkgs.nodePackages.prettier
             ];
